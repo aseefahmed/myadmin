8accd6c664e23cce9167d8d54cb399e229cc6cc8
aseef ahmed <aseefahmed@gmail.com>
2016-10-01 00:32:40 +0600

    Creat Dashboard Controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,34 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require js/jquery-migrate.min
+//= import js/jquery-migrate
 //= require turbolinks
-//= require lib/jquery-ui/jquery-ui-1.10.0.custom.min
-//= require js/forms/jquery.ui.touch-punch.min
-//= require js/jquery.easing.1.3.min
-//= require js/jquery.debouncedresize.min
-//= require js/jquery_cookie_min
-//= require bootstrap/js/bootstrap.min
-//= require js/bootstrap.plugins.min
-//= require lib/typeahead/typeahead.min
-//= require lib/google-code-prettify/prettify.min
-//= require lib/sticky/sticky.min
-//= require lib/colorbox/jquery.colorbox.min
-//= require lib/jBreadcrumbs/js/jquery.jBreadCrumb.1.1.min
-//= require js/jquery.actual.min
-//= require lib/slimScroll/jquery.slimscroll
-//= require js/ios-orientationchange-fix
-//= require lib/UItoTop/jquery.ui.totop.min
-//= require lib/moment/moment.min
-//= require js/selectNav
-//= require js/pages/gebo_common
-//= require js/jquery.imagesloaded.min
-//= require js/jquery.wookmark
-//= require js/jquery.mediaTable.min
-//= require js/jquery.peity.min
-//= require lib/flot.tooltip/jquery.flot.tooltip.min
-//= require lib/fullcalendar/fullcalendar.min
-//= require lib/list_js/list.min
-//= require lib/list_js/plugins/paging/list.paging.min
-//= require js/pages/gebo_dashboard
-//= require_tree .
+//= require_self .

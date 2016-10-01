024d0bc9684aa847ac1c6a34e9f692c6fea253e4
aseef ahmed <aseefahmed@gmail.com>
2016-10-01 14:01:42 +0600

    Create migration classes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,15 +13,33 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap/js/bootstrap.min
 //= require lib/datatables/jquery.dataTables.min
 //= require lib/datatables/extras/Scroller/media/js/dataTables.scroller.min
 //= require lib/datatables/extras/TableTools/media/js/TableTools.min
-//= require bootstrap/js/bootstrap.min
+//= require lib/datatables/extras/TableTools/media/js/ZeroClipboard
+//= require lib/datatables/jquery.dataTables.bootstrap.min
+
 //= require_self
 
 var ready;
 ready = function(){
-    $('#data-table').dataTable();
+    $('#data-table').dataTable({
+        "sPaginationType": "bootstrap",
+        "sDom": "<'row'<'col-sm-4'l><'col-sm-4 text-right'T><'col-sm-4'f>r>t<'row'<'col-sm-5'i><'col-sm-7'p>>",
+        "oTableTools": {
+            "aButtons": [
+                "copy",
+                "print",
+                {
+                    "sExtends":    "collection",
+                    "sButtonText": 'Save <span class="caret" />',
+                    "aButtons":    [ "csv", "xls", "pdf" ]
+                }
+            ],
+            "sSwfPath": "lib/datatables/extras/TableTools/media/swf/copy_csv_xls_pdf.swf"
+        }
+    });;
 }
 
 $(document).ready(ready);

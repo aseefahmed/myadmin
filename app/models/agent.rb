8aecef39bc11474eb25a597e475688a655e6def0
Aseef Ahmed <aseef.ahmed@welltravel.com>
2016-10-06 13:24:56 +0600

    COMMIT

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,4 +1,4 @@
 class Agent < ApplicationRecord
-  has_attached_file :image, path: ":rails_root/public/images/uploads/:class/:id/:style/:filename", styles: { medium: "300x300", thumb: "100x100" }
+  has_attached_file :image, path: ":rails_root/public/assets/uploads/:class/:id/:style/:filename", styles: { medium: "300x300", thumb: "100x100" }
   validates_attachment :image, content_type: {content_type: ["image/jpeg", "image/gif", "image/png"]}
 end

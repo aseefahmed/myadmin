f6558587b0e4c076407d0aa614f56568217d7956
ASEEF AHMED <aseefahmed@gmail.com>
2016-10-07 23:31:35 +0600

    COMMIT

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,4 @@
 class Brand < ApplicationRecord
-  has_attached_file :image, path: ":rails_root/public/images/uploads/:class/:id/:style/:filename", styles: { medium: "300x300", thumb: "100x100" }
+  has_attached_file :image, path: ":rails_root/public/assets/images/uploads/:class/:id/:style/:filename", styles: { medium: "300x300", thumb: "100x100" }
   validates_attachment :image, content_type: {content_type: ["image/jpeg", "image/gif", "image/png"]}
 end
